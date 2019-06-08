c31a50bb10b4bcae559cc5e8347e35dc5e9f9386
Keinick <pipin0715@gmail.com>
2019-06-08 13:33:26 +0900

    Binding pry

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   if Rails.env.production? 
     config.storage = :fog
     config.fog_provider = 'fog/aws'
+binding.pry
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
