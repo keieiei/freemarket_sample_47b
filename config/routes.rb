0cb3d081fd68cbe008dffb05f9770d6b3fd63d5f
kurogoro <kurogoro0241@gmail.com>
2019-06-09 17:59:03 +0900

    Customize Devise views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  }
+  devise_scope :user do
+    get "sign_up", :to => "users/registrations#new"
+    post "sign_up", :to => "users/registrations#create"
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'test#index'
+  resources :users, only: [:index, :edit, :update]
 end
