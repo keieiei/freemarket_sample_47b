5f2983a4bb1f7c5235589e1074636385d51b011d
Keinick <pipin0715@gmail.com>
2019-06-22 16:06:12 +0900

    Create_item_controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,27 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+
+  end
+  
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      puts"ほぞんできてる"
+      redirect_to root_path
+    else
+      puts"ほぞんできていない"
+      redirect_to root_path
+    end
+  end
+  
+  private
+  def items_params
+    params.require(:item).permit(:name, :detail,:state,:delivery_charge,:delivery_prefecture,:delivery_time,:delivery_way,:price,:size,:large_category,:middle_category,:small_category, :brand, images_attributes: [:image])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     get "sign_out", :to => "users/sessions#destroy" 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'test#index'
+  root 'items#index'
   resources :users, only: [:index, :edit, :update]
+  resources :items, only: [:index, :new, :create]
 end
