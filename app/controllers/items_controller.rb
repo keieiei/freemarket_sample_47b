c3343ef5bc2cd13f0f30e9c2de0128f6ba14842e
Keinick <pipin0715@gmail.com>
2019-06-22 18:49:55 +0900

    Change_Redirect

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,10 +15,10 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       puts"ほぞんできていない"
-      redirect_to root_path
+      redirect_to action: 'new'
     end
   end
-  
+
   private
   def items_params
     params.require(:item).permit(:name, :detail,:state,:delivery_charge,:delivery_prefecture,:delivery_time,:delivery_way,:price,:size,:large_category,:middle_category,:small_category, :brand, images_attributes: [:image])
