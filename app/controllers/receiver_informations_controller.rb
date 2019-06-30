8c8c8bace85940f45de515d4866d3c89be4df8b7
kurogoro <kurogoro0241@gmail.com>
2019-06-30 19:02:04 +0900

    Add create action

--- app/controllers/receiver_informations_controller.rb
+++ app/controllers/receiver_informations_controller.rb
@@ -4,4 +4,17 @@ class ReceiverInformationsController < ApplicationController
   def new
     @receiver_information = ReceiverInformation.new
   end
+
+  def create
+    @receiver_information = ReceiverInformation.new(receiver_information_params)
+    return render :new unless @receiver_information.valid?
+    @receiver_information.save
+    redirect_to new_user_credit_card_path(current_user.id)
+  end
+
+  private
+
+  def receiver_information_params
+    params.require(:receiver_information).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :street, :building, :phone_number).merge(user_id: current_user.id)
+  end
 end
