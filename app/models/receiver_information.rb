1e6f19fbe43887b40e7ef3334f750f8291308121
kurogoro <kurogoro0241@gmail.com>
2019-06-30 18:24:27 +0900

    Modify validation of receiver_information model

--- app/models/receiver_information.rb
+++ app/models/receiver_information.rb
@@ -1,2 +1,20 @@
 class ReceiverInformation < ApplicationRecord
+  validates :family_name, presence: true,
+                          zenkaku: true
+  validates :first_name, presence: true,
+                         zenkaku: true
+  validates :family_name_kana, presence: true,
+                               zenkaku_kana: true
+  validates :first_name_kana, presence: true,
+                              zenkaku_kana: true
+  validates :postal_code, presence: true,
+                          postal_code: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+  validates :phone_number, telphone: true, allow_blank: true
+
+  enum prefecture: I18n.t('prefecture')
+
+  belongs_to :user
 end
