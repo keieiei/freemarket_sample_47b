92f7203033385af91286144b6e6dbf736c3edb7a
kurogoro <kurogoro0241@gmail.com>
2019-06-30 20:35:49 +0900

    Set helper for progress-bar of user-sign-up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,36 @@ module ApplicationHelper
     end
     return selection
   end
+
+  def progress_bar_item_name(i, page_number, base_name)
+    active_name = base_name + "--active"
+    item_name = []
+    item_name << base_name
+    if i == page_number - 1 then
+      item_name << active_name
+    end
+    return item_name
+  end
+
+  def progress_bar_status_name(i, page_number, base_name)
+    active_name = base_name + "--active"
+    before_name = base_name + "--before"
+    first_name = base_name + "--first"
+    last_name = base_name + "--last"
+    status_name = []
+    status_name << base_name
+    if i == 0 then
+      status_name << first_name
+    end
+    if i == 4 then
+      status_name << last_name
+    end
+    if i == page_number - 1 then
+      status_name << active_name
+    end
+    if i < page_number - 1 then
+      status_name << before_name
+    end
+    return status_name
+  end
 end
