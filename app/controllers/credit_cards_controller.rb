9cb1d492bfa6524bbddfd43936fdb3d739e85cd0
kurogoro <kurogoro0241@gmail.com>
2019-06-30 19:34:15 +0900

    Set new and create action for credit_card controller

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,18 @@
 class CreditCardsController < ApplicationController
   def new
+    @credit_card = CreditCard.new
+  end
+
+  def create
+    @credit_card = CreditCard.new(credit_card_params)
+    return render :new unless @credit_card.valid?
+    @credit_card.save
+    redirect_to users_sign_up_complete_path
+  end
+
+  private
+
+  def credit_card_params
+    params.require(:credit_card).permit(:card_number, :valid_month, :valid_year, :security_code).merge(user_id: current_user.id)
   end
 end
