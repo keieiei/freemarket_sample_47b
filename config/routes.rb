a35a97e687673633fb5887a104550444e512b674
kurogoro <kurogoro0241@gmail.com>
2019-07-06 19:49:13 +0900

    Create sign-in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
   }
   devise_scope :user do
     post 'users/sign_up/confirm' => 'users/registrations#confirm'
