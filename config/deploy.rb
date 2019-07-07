4ed011ca8266f6e9fb4928a9907f0ddc199ca34c
Keinick <pipin0715@gmail.com>
2019-07-07 15:07:58 +0900

    linked_filesの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'freemarket_sample_47B'
 set :repo_url,  'git@github.com:keieiei/freemarket_sample_47B.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml' 'config/master.key')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
@@ -16,8 +16,6 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
