c424c9f511db0d0457d3edc7b51230acee1245f3
kurogoro <kurogoro0241@gmail.com>
2019-07-20 18:59:26 +0900

    Merge 2 migration files

--- db/migrate/20190720090314_add_columns_to_items.rb
+++ db/migrate/20190720090314_add_columns_to_items.rb
@@ -1,5 +1,7 @@
 class AddColumnsToItems < ActiveRecord::Migration[5.2]
   def change
     add_column :items, :item_condition_id, :integer, null: false
+    rename_column :items, :sell_id, :seller_id
+    rename_column :items, :buy_id, :buyer_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_092634) do
+ActiveRecord::Schema.define(version: 2019_07_20_090314) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
