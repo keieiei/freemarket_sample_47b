39741c5722b00ac76100066ccc8c650e56ef5fb6
kurogoro <kurogoro0241@gmail.com>
2019-07-20 18:59:05 +0900

    Modify custum validator

--- app/validators/small_category_validator.rb
+++ app/validators/small_category_validator.rb
@@ -4,10 +4,8 @@ class SmallCategoryValidator < ActiveModel::Validator
     small_category_id = record.send(options[:small_category_id])
     small_category_ids = SmallCategory.where(middle_category_id: middle_category_id).ids
 
-    unless small_category_ids.empty? && small_category_id.nil?
-      unless small_category_ids.include?(small_category_id)
-        record.errors.add(:base, 'DBに登録されているidを入力してください')
-      end
+    unless small_category_ids.empty? && small_category_id.nil? && small_category_ids.include?(small_category_id)
+      record.errors.add(:base, 'DBに登録されているidを入力してください')
     end
   end
 end
