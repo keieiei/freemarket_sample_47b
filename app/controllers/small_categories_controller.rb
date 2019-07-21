06517d8c05812d5044362a0b74ee064cd1dd042e
kurogoro <kurogoro0241@gmail.com>
2019-07-21 14:54:22 +0900

    Create small_categories show action

--- app/controllers/small_categories_controller.rb
+++ app/controllers/small_categories_controller.rb
@@ -1,4 +1,23 @@
 class SmallCategoriesController < ApplicationController
+  before_action :set_large_categories, only: [:show]
+  before_action :set_brands, only: [:show]
+
   def show
+    @small_category = SmallCategory.find(show_params[:id])
+    @items = @small_category.items.order("created_at DESC").page(params[:page]).per(5)
+    @nav_lists = nav_lists(@small_category.middle_category, "small")
+  end
+
+  private
+  def show_params
+    params.permit(:id)
+  end
+
+  def set_large_categories
+    @large_categories = LargeCategory.all
+  end
+
+  def set_brands
+    @brands = Brand.all
   end
 end
