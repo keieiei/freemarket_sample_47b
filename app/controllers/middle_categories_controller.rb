0b986ab1c7fa6db185e947717d2138770988bcd6
kurogoro <kurogoro0241@gmail.com>
2019-07-21 13:56:33 +0900

    Add  middle_category show  action

--- app/controllers/middle_categories_controller.rb
+++ app/controllers/middle_categories_controller.rb
@@ -1,4 +1,22 @@
 class MiddleCategoriesController < ApplicationController
+  before_action :set_large_categories, only: [:show]
+  before_action :set_brands, only: [:show]
   def show
+    @middle_category = MiddleCategory.find(show_params[:id])
+    @items = @middle_category.items.order("created_at DESC").page(params[:page]).per(5)
+    @nav_lists = nav_lists(@middle_category, "middle")
+  end
+
+  private
+  def show_params
+    params.permit(:id)
+  end
+
+  def set_large_categories
+    @large_categories = LargeCategory.all
+  end
+
+  def set_brands
+    @brands = Brand.all
   end
 end
