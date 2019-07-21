4dbaed3df48e48448144b747373a774777409ffe
kurogoro <kurogoro0241@gmail.com>
2019-07-21 16:07:54 +0900

    Create brand show action

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,6 +1,25 @@
 class BrandsController < ApplicationController
+  before_action :set_large_categories, only: [:index, :show]
+  before_action :set_brands, only: [:index, :show]
+
   def index
   end
   def show
+    @brand = Brand.find(show_params[:id])
+    @items = @brand.items.order("created_at DESC").page(params[:page]).per(5)
+    @nav_lists = nav_lists(@brand, "brand")
+  end
+
+  private
+  def show_params
+    params.permit(:id)
+  end
+
+  def set_large_categories
+    @large_categories = LargeCategory.all
+  end
+
+  def set_brands
+    @brands = Brand.all
   end
 end
