18a26afa09a4a0177ea903ef51d3c66c06e1d24e
kurogoro <kurogoro0241@gmail.com>
2019-07-26 18:46:18 +0900

    Modify "if" sentences in validates

--- app/validators/brand_validator.rb
+++ app/validators/brand_validator.rb
@@ -4,22 +4,22 @@ class BrandValidator < ActiveModel::Validator
     middle_category_id = record.send(options[:middle_category_id])
     small_category_id = record.send(options[:small_category_id])
 
-    if !middle_category_id.nil?
+    unless middle_category_id.nil?
       brand_upper_category_m = MiddleCategory.find(middle_category_id).brand_upper_category
-      if !brand_upper_category_m.nil?
-        if !brand_id.nil?
+      unless brand_upper_category_m.nil?
+        unless brand_id.nil?
           brand_upper_category_b = Brand.find(brand_id).brand_upper_category
-          return record.errors.add(:base, 'ブランド種類が異なる(m)') if brand_upper_category_b != brand_upper_category_m
+          return record.errors.add(:base, 'ブランド種類が異なる(m)') unless brand_upper_category_b == brand_upper_category_m
         end
       end
-      if !small_category_id.nil?
+      unless small_category_id.nil?
         brand_upper_category_s = SmallCategory.find(small_category_id).brand_upper_category
         if brand_upper_category_s.nil?
-          return record.errors.add(:base, 'ブランド登録未許可') if !brand_id.nil?
+          return record.errors.add(:base, 'ブランド登録未許可') unless brand_id.nil?
         else
-          if !brand_id.nil?
+          unless brand_id.nil?
             brand_upper_category_b = Brand.find(brand_id).brand_upper_category
-            return record.errors.add(:base, 'ブランド種類が異なる(s)') if brand_upper_category_b != brand_upper_category_s
+            return record.errors.add(:base, 'ブランド種類が異なる(s)') unless brand_upper_category_b == brand_upper_category_s
           end
         end
       end
--- app/validators/item_size_validator.rb
+++ app/validators/item_size_validator.rb
@@ -4,21 +4,21 @@ class ItemSizeValidator < ActiveModel::Validator
     middle_category_id = record.send(options[:middle_category_id])
     small_category_id = record.send(options[:small_category_id])
 
-    if !middle_category_id.nil?
+    unless middle_category_id.nil?
       size_type_m = MiddleCategory.find(middle_category_id).size_type
-      if !size_type_m.nil?
+      unless size_type_m.nil?
         return record.errors.add(:base, 'サイズ未登録') if item_size_id.nil?
         size_type_i = ItemSize.find(item_size_id).size_type
-        return record.errors.add(:base, 'サイズ種類が異なる') if size_type_i != size_type_m
+        return record.errors.add(:base, 'サイズ種類が異なる') unless size_type_i == size_type_m
       end
-      if !small_category_id.nil?
+      unless small_category_id.nil?
         size_type_s = SmallCategory.find(small_category_id).size_type
         if size_type_s.nil?
-          return record.errors.add(:base, 'サイズ登録未許可') if !item_size_id.nil?
+          return record.errors.add(:base, 'サイズ登録未許可') unless item_size_id.nil?
         else
           return record.errors.add(:base, 'サイズ未登録') if item_size_id.nil?
           size_type_i = ItemSize.find(item_size_id).size_type
-          return record.errors.add(:base, 'サイズ種類が異なる') if size_type_i != size_type_s
+          return record.errors.add(:base, 'サイズ種類が異なる') unless size_type_i == size_type_s
         end
       end
     end
