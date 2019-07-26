818fe32521969e5699a36ff2b645149f0a7948e8
kurogoro <kurogoro0241@gmail.com>
2019-07-26 19:12:04 +0900

    Modify migration file

--- db/migrate/20190723131159_delete_brand_large_categories.rb
+++
@@ -1,7 +0,0 @@
-class DeleteBrandLargeCategories < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :brand_large_categories
-    drop_table :brand_middle_categories
-    drop_table :brand_small_categories
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_131159) do
+ActiveRecord::Schema.define(version: 2019_07_21_155911) do
 
   create_table "brand_upper_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
