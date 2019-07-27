820455492b8052a04002ce98e3f17ef093dd9de4
kurogoro <kurogoro0241@gmail.com>
2019-07-27 14:21:39 +0900

    Remove brand_large_category validator

--- app/validators/brand_large_category_validator.rb
+++
@@ -1,9 +0,0 @@
-class BrandLargeCategoryValidator < ActiveModel::Validator
-  def validate(record)
-    brand_id = record.send(options[:brand_id])
-    large_category_id = record.send(options[:large_category_id])
-    unless large_category_id.nil? && BrandLargeCategory.where(brand_id: brand_id).where(large_category_id: large_category_id).empty?
-      record.errors.add(:base, 'DBに登録済の組み合わせ')
-    end
-  end
-end
