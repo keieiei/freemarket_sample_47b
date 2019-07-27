0db748e31c71a8c2741bdce5b40579350bf52eb5
kurogoro <kurogoro0241@gmail.com>
2019-07-27 14:24:34 +0900

    Change validatos

--- app/validators/brand_small_category_validator.rb
+++ app/validators/brand_small_category_validator.rb
@@ -2,8 +2,15 @@ class BrandSmallCategoryValidator < ActiveModel::Validator
   def validate(record)
     brand_id = record.send(options[:brand_id])
     small_category_id = record.send(options[:small_category_id])
-    unless small_category_id.nil? && BrandSmallCategory.where(brand_id: brand_id).where(small_category_id: small_category_id).empty?
-      record.errors.add(:base, 'DBに登録済の組み合わせ')
+    unless small_category_id.nil?
+      brand = Brand.find(brand_id)
+      small_category = SmallCategory.find(small_category_id)
+      unless BrandSmallCategory.where(brand_id: brand_id).where(small_category_id: small_category_id).empty?
+        return record.errors.add(:base, '既にその組み合わせは登録済')
+      end
+      unless brand.brand_brand_upper_categories.pluck(:brand_upper_category_id).include?(small_category.brand_upper_category.id)
+        record.errors.add(:base, 'brand_upper_categoryが異なるため登録不可')
+      end
     end
   end
 end
