5037bdc5a559aa60603b9fdf169d941ad2c645c6
kurogoro <kurogoro0241@gmail.com>
2019-07-27 14:24:55 +0900

    Change common methods for controller

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -1,40 +1,34 @@
 module Common
   extend ActiveSupport::Concern
 
-  def large_nav_lists(target)
+  def large_nav_lists(large_category)
     lists = []
-    target.middle_categories.each do |middle_category|
+    large_category.middle_categories.each do |middle_category|
       lists << {name: middle_category.name, link: middle_category_path(middle_category.id)}
     end
     return lists
   end
 
-  def middle_nav_lists(target)
+  def middle_nav_lists(middle_category)
     lists = []
-    target.small_categories.each do |small_category|
+    middle_category.small_categories.each do |small_category|
       lists << {name: small_category.name, link: small_category_path(small_category.id)}
     end
     return lists
   end
 
-  def brand_nav_lists(target)
+  def brand_nav_lists(brand)
     lists = []
-    if !target.brand_small_categories.empty?
-      target.brand_small_categories.each do |brand_small_category|
+    unless brand.brand_small_categories.empty?
+      brand.brand_small_categories.each do |brand_small_category|
         lists << {name: brand_small_category.small_category.name, link: small_category_path(brand_small_category.small_category.id)}
       end
       return lists
-      if !target.brand.middle_categories.empty?
-        target.brand_middle_categories.each do |brand_middle_category|
+      unless brand.brand_middle_categories.empty?
+        brand.brand_middle_categories.each do |brand_middle_category|
           lists << {name: brand_middle_category.middle_category.name, link: middle_category_path(brand_middle_category.middle_category.id)}
         end
         return lists
-        if !target.brand_large_categories.empty?
-          target.brand_large_categories.each do |brand_large_category|
-            lists << {name: brand_large_category.large_category.name, link: large_category_path(brand_large_category.large_category.id)}
-          end
-          return lists
-        end
       end
     end
   end
