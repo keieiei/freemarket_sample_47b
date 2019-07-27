fbed4edcf476fd5f832a1d0a94a2c3f9bc6776ab
kurogoro <kurogoro0241@gmail.com>
2019-07-27 15:14:58 +0900

    Modify brand_middle_category_validator

--- app/validators/brand_middle_category_validator.rb
+++ app/validators/brand_middle_category_validator.rb
@@ -8,7 +8,7 @@ class BrandMiddleCategoryValidator < ActiveModel::Validator
       unless BrandMiddleCategory.where(brand_id: brand_id).where(middle_category_id: middle_category_id).empty?
         return record.errors.add(:base, '既にその組み合わせは登録済')
       end
-      unless brand.brand_brand_upper_category.pluck(:brand_upper_category_id).include?(middle_category.brand_upper_category.id)
+      unless brand.brand_brand_upper_categories.pluck(:brand_upper_category_id).include?(middle_category.brand_upper_category.id)
         record.errors.add(:base, 'brand_upper_categoryが異なるため登録不可')
       end
     end
