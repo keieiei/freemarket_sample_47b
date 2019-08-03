ce0a2e0ab1bb9c8f85bf6d8cd129ba992db48ad4
kurogoro <kurogoro0241@gmail.com>
2019-08-03 15:56:22 +0900

    Modify item_state.rb

--- app/models/item_state.rb
+++ app/models/item_state.rb
@@ -1,9 +1,6 @@
 class ItemState < ActiveHash::Base
   self.data = [
-    {id: 1, name: '出品中'},
-    {id: 2, name: '配達前'},
-    {id: 3, name: '配達中'},
-    {id: 4, name: '配達済'},
-    {id: 5, name: '評価済'}
+    {id: 1, name: '販売中'},
+    {id: 4, name: '売り切れ'}
   ]
 end
