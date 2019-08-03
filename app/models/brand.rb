7e4e254bde4c13910a66b3f26216ff4a376c2387
kurogoro <kurogoro0241@gmail.com>
2019-08-04 08:22:00 +0900

    Modify brand and brand_upper_category association

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ApplicationRecord
   has_many :items
-  has_many :brand_upper_categories, through: :brand_brand_upper_categories
   has_many :brand_brand_upper_categories
+  has_many :brand_upper_categories, through: :brand_brand_upper_categories
   has_many :brand_middle_categories
   has_many :middle_categories, through: :brand_middle_categories
   has_many :brand_small_categories
--- app/models/brand_upper_category.rb
+++ app/models/brand_upper_category.rb
@@ -1,6 +1,6 @@
 class BrandUpperCategory < ApplicationRecord
-  has_many :brands, through: :brand_brand_upper_categories
   has_many :brand_brand_upper_categories
+  has_many :brands, through: :brand_brand_upper_categories
   has_many :small_categories
   has_many :middle_categories
 end
